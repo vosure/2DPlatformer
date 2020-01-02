73e617c4ce8074a18260f7ed512cf4c24763474a
vosure <vosure1@gmail.com>
2020-01-02 22:04:57 +0300

    initial commit. set everything. added controller and player scripts

---
+++ Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    Controller2D controller;
+
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
