67d0fa9769dc61a55c6e83e299fa7c95b03f5170
vosure <vosure1@gmail.com>
2020-01-04 21:56:15 +0300

    vertical and horizontal collision detection

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
+    float moveSpeed = 6;
+    float gravity = -20;
+    Vector3 velocity;
+
     Controller2D controller;
 
     void Start()
@@ -14,6 +18,10 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
